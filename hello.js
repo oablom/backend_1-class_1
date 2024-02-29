ef070b804d40c588a3f679d8db9c388ef345343d
Oa Blom <oablom@gmail.com>
2024-02-29 11:47:54 +0100

    added material and args

--- hello.js
+++ hello.js
@@ -1,2 +1,31 @@
-console.log("Hello, World!");
-let p = prompt("What is your name?");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const items = [];
+
+console.log(items);
+console.log(items.length);
+
+function addItemsToList() {
+  if (items.length === 5) {
+    rl.close();
+    console.log(items);
+    return console.log("List is full");
+  }
+  rl.question("Enter an item: ", function (product) {
+    items.push(item);
+    console.log("Items: ", items);
+    return addItemsToList();
+  });
+
+  const productIndex = items.findIndex((item) => item === product);
+  if (productIndex !== -1) {
+    console.log("Item already exists");
+    return addItemsToList();
+  }
+}
+addItemsToList();
